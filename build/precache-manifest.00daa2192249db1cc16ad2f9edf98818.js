be5e051bb3e9608bddc3f0d9894f6de5e3ddfef9
Henry Larson <hxl1116@g.rit.edu>
2019-10-07 17:53:24 -0400

    Added deployment packages. Removed .build directory from .gitignore.

---
+++ build/precache-manifest.00daa2192249db1cc16ad2f9edf98818.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec285a8b93091028e5d29b082c321b6",
+    "url": "/Conference/index.html"
+  },
+  {
+    "revision": "2543c3a8822b85ec4b51",
+    "url": "/Conference/static/js/2.356c9a28.chunk.js"
+  },
+  {
+    "revision": "e84973312c9f42d3f013",
+    "url": "/Conference/static/js/main.ac18b81e.chunk.js"
+  },
+  {
+    "revision": "3aa93a64654ca4fd608a",
+    "url": "/Conference/static/js/runtime-main.21f69255.js"
+  }
+]);
